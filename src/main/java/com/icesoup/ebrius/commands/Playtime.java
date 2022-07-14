9a51f8a980543fb5c9474cd1ecc22986724919fc
Sadeeed <sadeeedw@gmail.com>
2022-07-15 01:18:38 +0500

    :package: skeleton code for handling arguments

--- src/main/java/com/icesoup/ebrius/commands/Playtime.java
+++ src/main/java/com/icesoup/ebrius/commands/Playtime.java
@@ -18,10 +18,9 @@ public class Playtime implements CommandExecutor {
                 (TimeUnit.SECONDS.toHours(seconds) * 60));
         long secs = (TimeUnit.SECONDS.toSeconds(seconds) -
                 (TimeUnit.SECONDS.toMinutes(seconds) * 60));
-        if (days > 0){
+        if (days > 0) {
             return days + "d" + " " + hours + "h" + " " + minutes + "m" + " " + secs + "s";
-        }
-        else {
+        } else {
             return hours + "h" + " " + minutes + "m" + " " + secs + "s";
         }
     }
@@ -30,10 +29,14 @@ public class Playtime implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            int secondsPlayed = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
-            System.out.println(secondsPlayed);
-            String timePlayed = ChatColor.AQUA + "Time Played: " + ChatColor.RESET + getTimeString(secondsPlayed);
-            player.sendMessage(timePlayed);
+            if (args.length > 0) {
+                player.sendMessage("Hello: " + args[0]);
+            } else {
+                int secondsPlayed = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+                System.out.println(secondsPlayed);
+                String timePlayed = ChatColor.AQUA + "Time Played: " + ChatColor.RESET + getTimeString(secondsPlayed);
+                player.sendMessage(timePlayed);
+            }
             return true;
         }
         return false;
