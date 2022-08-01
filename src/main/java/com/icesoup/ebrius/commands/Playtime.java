093ab12f955cd86b96780aa34e95c4407d80bb5a
Sadeeed <sadeeedw@gmail.com>
2022-08-02 01:52:18 +0500

    :package: get playtime for offline players

--- src/main/java/com/icesoup/ebrius/commands/Playtime.java
+++ src/main/java/com/icesoup/ebrius/commands/Playtime.java
@@ -2,12 +2,14 @@ package com.icesoup.ebrius.commands;
 
 import com.icesoup.ebrius.Main;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 //import java.util.logging.Level;
 
@@ -35,23 +37,45 @@ public class Playtime implements CommandExecutor {
 
     private void getPlaytime(Player player, Player sender) {
         int secondsPlayed = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
-        System.out.println(secondsPlayed);
-        String timePlayed = ChatColor.AQUA + "Time Played: " + ChatColor.RESET + getTimeString(secondsPlayed);
+        String startString = player.getName() + "'s Playtime: ";
+        if (player.equals(sender)){
+            startString = "Time Played: ";
+        }
+        String timePlayed = ChatColor.AQUA + startString + ChatColor.RESET + getTimeString(secondsPlayed);
+        sender.sendMessage(timePlayed);
+    }
+
+    private void getPlaytime(OfflinePlayer player, Player sender) {
+        int secondsPlayed = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+        String startString = player.getName() + "'s Playtime: ";
+        String timePlayed = ChatColor.AQUA + startString + ChatColor.RESET + getTimeString(secondsPlayed);
         sender.sendMessage(timePlayed);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        boolean foundOffline = false;
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length > 0) {
-                player.sendMessage("Hello: " + args[0]);
-                Player player1 = plugin.getServer().getPlayer(args[0]);
+//                player.sendMessage("Hello: " + args[0]);
+                Player player1 = plugin.getServer().getPlayerExact(args[0]);
                 if (player1 != null) {
-                    plugin.getLogger().info(player1.getName());
+//                    plugin.getLogger().info(player1.getName());
                     getPlaytime(player1, player);
                 } else {
-                    plugin.getLogger().info("Player not found");
+                    OfflinePlayer[] offlinePlayers = plugin.getServer().getOfflinePlayers();
+                    for (OfflinePlayer offlinePlayer:offlinePlayers) {
+                        if(Objects.equals(offlinePlayer.getName(), args[0])){
+//                            plugin.getLogger().info(offlinePlayer.getName());
+                            getPlaytime(offlinePlayer, player);
+                            foundOffline = true;
+                            break; // break out of the loop when player is found offline
+                        }
+                    }
+                    if (!foundOffline){
+                        player.sendMessage(ChatColor.RED + "Player not found!");
+                    }
                 }
 
             } else {
